1a93152bb1a2d22ef620aabf9942408eaa5e768d
jmbae7 <jmbae7@gmail.com>
2021-09-17 17:45:24 +0900

    'update'

---
+++ ProviderPattern/ProviderPatternLogTest/LogProvider/Providers/LogProviderBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration.Provider;
+
+namespace ProviderPatternLogTest.LogProvider.Providers
+{
+    public abstract class LogProviderBase : ProviderBase
+    {
+
+        public virtual void SetParameters(NameValueCollection config)
+        {
+
+        }
+
+        public virtual void Info(string message)
+        {
+            WriteLog(LogType.Info, message);
+        }
+
+        public virtual void Warning(string message)
+        {
+            WriteLog(LogType.Warning, message);
+        }
+
+        public virtual void Error(string message)
+        {
+            WriteLog(LogType.Error, message);
+        }
+
+        public abstract void WriteLog(LogType logType, string message);
+        
+    }
+}
